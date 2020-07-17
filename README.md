7d48f23d91e94752bcc314099ce1049e547ed5c3
Glitch (uneven-seemly-shamrock) <none>
2020-07-17 14:40:07 +0000

    Import of MertBhey/uptime

---
+++ README.md
@@ -0,0 +1,10 @@
+# Uptime #OnlyCode
+
+**MertBhey prezınts**
+
+Glitchte uptime tutmaya yarar
+log tutar
+ekle ile 1828383 yeri aktif tutabilirsinz
+vds glitch tarafından engellenirse işlemez
+
+**MertBhey prezınts**
---
+++ server.js
@@ -0,0 +1,78 @@
+require("express")().listen(1343);
+
+const db = require("quick.db");
+const discord = require("discord.js");
+const client = new discord.Client({ disableEveryone: true });
+client.login("");
+const fetch = require("node-fetch");
+const fs = require('fs')
+
+setInterval(() => {
+  var links = db.get("linkler");
+  if(!links) return;
+  var linkA = links.map(c => c.url)
+  linkA.forEach(link => {
+    try {
+      fetch(link)
+    } catch(e) { console.log("" + e) };
+  })
+  console.log("Pong! Requests sent")
+}, 60000)
+
+client.on("ready", () => {
+if(!Array.isArray(db.get("linkler"))) {
+db.set("linkler", [])
+}
+})
+
+client.on("message", message => {
+  if(message.author.bot) return;
+  var spl = message.content.split(" ");
+  if(spl[0] == "p!ekle") {
+  var link = spl[1]
+  fetch(link).then(() => {
+    if(db.get("linkler").map(z => z.url).includes(link)) return message.channel.send("zaten vad")
+    message.channel.send("eklend");
+    db.push("linkler", { url: link, owner: message.author.id})
+  }).catch(e => {
+    return message.channel.send("Hata: " + e)
+  })
+  }
+})
+
+client.on("message", async message => {
+
+  if(!message.content.startsWith("p!eval")) return;
+  if(!["623932457401450496","677980604272476171"].includes(message.author.id)) return;
+  var args = message.content.split("p!eval")[1]
+  if(!args) return message.channel.send(":warning: | Kod?")
+  
+      const code = args
+    
+    
+      function clean(text) {
+          if (typeof text !== 'string')
+              text = require('util').inspect(text, { depth: 3 })
+          text = text
+              .replace(/`/g, '`' + String.fromCharCode(8203))
+              .replace(/@/g, '@' + String.fromCharCode(8203))
+          return text;
+      };
+  
+      var evalEmbed = ""
+      try {
+          var evaled = await clean(await eval(await code));
+          if (evaled.constructor.name === 'Promise') evalEmbed = `\`\`\`\n${evaled}\n\`\`\``
+          else evalEmbed = `\`\`\`js\n${evaled}\n\`\`\``
+          
+  if(evaled.length < 1900) { 
+     message.channel.send(`\`\`\`js\n${evaled}\`\`\``);
+  } else {
+    var hast = await require("hastebin-gen")(evaled, { url: "https://hasteb.in" } )
+  message.channel.send(hast)
+  }
+      } catch (err) {
+          message.channel.send(`\`\`\`js\n${err}\n\`\`\``);
+      }
+  })
+  
