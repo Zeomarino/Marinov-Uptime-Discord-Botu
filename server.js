3786d2bda80d26d1f04ccae7ab032febac2680c5
Glitch (uneven-seemly-shamrock) <none>
2020-08-08 13:38:42 +0000

    🗼🌥 Checkpoint ./server.js:13117774/192

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("express")().listen(1343);
 const db = require("quick.db"); 
 const discord = require("discord.js");
 const client = new discord.Client({ disableEveryone: true });
-client.login("NzQxNjQ0NjU3NzYyNzYyNzYz.Xy6kaQ.PJjFijOgIQ4P3bCwS4YqJ1y31do");
+client.login("Token");
 const fetch = require("node-fetch");
 const fs = require('fs')
 
