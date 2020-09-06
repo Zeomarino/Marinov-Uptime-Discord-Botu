c2e89a29708e3b2655c751eb84b181ec0e6eb1cb
Glitch (uneven-seemly-shamrock) <none>
2020-09-06 20:04:24 +0300

    project marinovuptimebot

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("express")().listen(1343);
 const db = require("quick.db"); 
 const discord = require("discord.js");
 const client = new discord.Client({ disableEveryone: true });
-client.login("NzQzMTk0NDE5MjA0ODQ5NzE2.XzRHvQ.OMzPgAdzY06qTYk6cB51zkgn9qE");
+client.login("Token HERE - Token Gir");
 const fetch = require("node-fetch");
 const fs = require('fs')
 
